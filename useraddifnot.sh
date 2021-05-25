6ef16dc3f0899d0996d24ec6aec9f9fa45cf1ea2
Ghansham Mahajan <51263352+GhanshamMahajan@users.noreply.github.com>
2021-05-25 12:01:49 +0530

    added the root verify script
    Added the code for verify the if script running by root or not.

--- useraddifnot.sh
+++ useraddifnot.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # Author : Ghansham Mahajan
-# Use : Create User if not exist.
+# Use : Apply the password policy for existing user
 
-user="$(cat /etc/passwd | grep 'bash' | cut -d: -f1 | grep -i 'ghansham')"
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2  				# Check if script running by root or not
+   exit 1									#If it's not run by root then it will exit the script
+fi
 
-if [ "$user" != "otisadmin" ] ; then
-	useradd -m -c "Linux User | $(date +%m-%d-%Y)" -s /bin/bash ghansham
-	usermod -aG sudo otisadmin
-	usermod -aG wheel otisadmin
-	echo "ghansham:@g4&[8XBKau%ha+<" | chpasswd
+user="$(cat /etc/passwd | grep 'bash' | cut -d: -f1 | grep -i 'ghansham')"	#Collect the user name in variable 
+
+if [ "$user" != "ghansham" ] ; then						# Verify if user is match or not
+    useradd -m -c "DevOps User | $(date +%m-%d-%Y)" -s /bin/bash ghansham	#If user is not match then useradd command get execute
+    usermod -aG sudo ghansham							#add the sudo group if it's Ubuntu
+    usermod -aG wheel ghansham							#add the sudo group if it's Ubuntu
+    echo "ghansham:]fyjVZLM@g4&[8XBKau%ha+<" | chpasswd				#Set the password for newly created user
 else 
     echo "$user already exist"  
 fi
